90616a463b9649d6c8826fd40d808e247f1230b9
martastaporek <staporekmarta1@gmail.com>
2018-05-20 16:55:21 +0200

    Implement add root to tree

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,24 +1,53 @@
+import java.util.*;
+
 public class Tree {
-    
 
-    private class Node{
+    private Node root;
+
+    private void buildTree(List<Integer> numbers) {
+
+        SortedSet<Integer> numbersSet = new TreeSet<>(numbers);
+        Integer first = numbersSet.first();
+        this.root = new Node(first);
+        numbersSet.remove(first);
+
+        for(Integer elem : numbersSet) {
+            Node toAdd = findNode(elem);
+        }
+
+    }
+
+    private Node findNode(Integer val) {
+        while (true) {
+            Node temp = this.root;
+            if (this.root.value > val) { // val na prawo
+                if (temp.children[0] != null && temp.children[1]){
+
+            }
+        }
+    }
+
+
+
+    private class Node {
         private Integer value;
-        private Node [] children = new Node[2];
+        private Node[] children = new Node[2];
 
-        private void Node(Integer value){
+        private Node(Integer value) {
             this.value = value;
         }
 
-        private void addChildren(Node child){ //call on object to which node is add
-            if(this.value > child.value){
+        private void addChildren(Node child) { //call on object to which node is add
+            if (this.value > child.value) {
                 this.children[0] = child;
-            }else{
+            } else {
                 this.children[1] = child;
             }
 
 
         }
     }
+}
 
 
 }
