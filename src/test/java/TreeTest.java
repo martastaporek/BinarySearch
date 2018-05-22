9fe32c4706bb691f07f01f8f42a4b2e9b945e104
martastaporek <staporekmarta1@gmail.com>
2018-05-22 11:38:15 +0200

    Add tests for findValue method

---
+++ src/test/java/TreeTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeTest {
+
+    private Tree tree;
+
+    @BeforeEach
+    void setup(){
+        this.tree = new Tree();
+    }
+
+    @Test
+    void findValueInRoot(){
+        this.tree.buildTree(new ArrayList<>(Arrays.asList(1, 9, 0, 8, 0, 4)));
+        assertTrue(this.tree.findValue(1));
+    }
+
+    @Test
+    void findValueInTheMiddle(){
+        this.tree.buildTree(new ArrayList<>(Arrays.asList(0, 9, 3, 9, 4)));
+        assertTrue(this.tree.findValue(4));
+    }
+
+    @Test
+    void findValueAtTheEnd(){
+        this.tree.buildTree(new LinkedList<>(Arrays.asList(1, 2, 9, 0, 5)));
+        assertTrue(this.tree.findValue(5));
+    }
+
+    @Test
+    void testIfValueIsNotInTree(){
+        this.tree.buildTree(new LinkedList<>(Arrays.asList(0, 9, 7, 5, 6)));
+        assertFalse(this.tree.findValue(102));
+    }
+
+
+}
